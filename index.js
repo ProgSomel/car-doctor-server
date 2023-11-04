931afbc94c185632d849cf87d3fdfa5bda7d1923
Somel <somelahmed88@gmail.com>
2023-11-04 10:24:53 +0600

    Save order using post method done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config(); 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT ||5000;
 
@@ -30,10 +30,31 @@ async function run() {
 
     const servicesCollection = client.db('carDoctor').collection('services');
 
+    const bookingCollection = client.db('carDoctor').collection('bookings');
+
 
     app.get('/services', async(req, res) => {
         const cursor = servicesCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/services/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const options = {
+            // Include only the `title` and `imdb` fields in each returned document
+            projection: {title: 1, price: 1,  service_id: 1},
+          };
+        const result = await servicesCollection.findOne(query, options);
+        res.send(result);
+    })
+
+    //! Bookings 
+    app.post('/bookings', async (req, res) => {
+        const booking = req.body;
+        console.log(booking);
+        const result = await bookingCollection.insertOne(booking);
         res.send(result);
     })
 
