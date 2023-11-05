b71cfe4ec33a996dcf2dd28bcf1ce52828135ad5
Somel <somelahmed88@gmail.com>
2023-11-05 09:46:32 +0600

    JWT token secret and set token to http only cookie

---
+++ _jwt_steps.js
@@ -0,0 +1,18 @@
+/**
+ * install jsonwebtoken
+ * jwt.sign(playload, secret, {expiresIn})
+ * token client
+ */
+
+/**
+ * how to store token in the client side
+ * 1. memory --> ok type
+ * 2. local storage --> ok type (xss)
+ * 3. cookies: http only
+ */
+
+/**
+ * 1. set cookies with http only. for development secure: false, 
+ * 2. cors
+ * 3. client side e axios diea setting set korte hobe
+ */
